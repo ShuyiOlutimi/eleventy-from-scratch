b84775ff6e885a6bbc631e9a5444c817045fd1bd
Olutimilehin Olushuyi <shuyi@shuyiolutimi.com>
2022-04-18 20:16:23 +0100

    move online

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
 module.exports = config => {
-    return {
-      dir: {
-        input: 'src',
-        output: 'dist'
-      }
-    };
-  }; 
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
+}; 
